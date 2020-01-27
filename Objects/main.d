8d428a46fad5a06bd4a56c76641d005396d00e36
Aleksey <sarlift@mail.ru>
2020-01-27 14:16:42 +0300

    deleted folders Signed-off-by: Aleksey <sarlift@mail.ru>

--- Objects/main.d
+++
@@ -1,3 +0,0 @@
-.\objects\main.o: main.c
-.\objects\main.o: tdcgp22.h
-.\objects\main.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- Objects/startup_stm32l496xx.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_stm32l496xx.o: RTE\Device\STM32L496VGTx\startup_stm32l496xx.s
